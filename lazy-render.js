246a626ea54bf9739be8f0656bb3cd84b569b9cd
Todd Kennedy <todd@selfassembled.org>
2017-01-26 11:39:37 -0800

    caching mountPoint for performance]

--- lazy-render.js
+++ lazy-render.js
@@ -1,12 +1,20 @@
 var React = require('react')
-var render = require('react-dom')
+var render = require('react-dom').render
+var _cache = Object.create(null)
 
 function lazyReact (mountPoint) {
   if (typeof mountPoint !== 'string') {
     throw new Error('mountPoint must be a string')
   }
+  if (!_cache[mountPoint]) {
+    _cache[mountPoint] = document.querySelector(mountPoint)
+  }
   return function lazyRender (component, props) {
-    render(React.createElement(component, props || {}, null), mountPoint)
+    var mount = _cache[mountPoint] || document.querySelector(mountPoint)
+    if (!mount) {
+      throw new Error(mountPoint + ' does not exist')
+    }
+    render(React.createElement(component, props || {}, null), mount)
   }
 }
 
